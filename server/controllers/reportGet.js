aa8459b0e628d3304bb3c7c9476fc18346a92bf3
Yash Ori <yash.ori498@gmail.com>
2022-06-14 20:50:25 +0100

    bug fixing

--- server/controllers/reportGet.js
+++ server/controllers/reportGet.js
@@ -1,8 +1,8 @@
 import Report from "../models/Report.js";
 
-export const getAllReports = (req, res) => {
+export const getAllReports = async (req, res) => {
     try {
-        const reports = Report.find();
+        const reports = await Report.find();
         return res.status(200).json(reports);
     } catch (error) {
         return res.status(500).send("No reports in database rn");
