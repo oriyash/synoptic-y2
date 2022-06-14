6c67a3a8f4b5982e910b8af5240037c078f30b12
Yash Ori <yash.ori498@gmail.com>
2022-06-14 18:46:32 +0100

    added pinia dep

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 
 // importing bootstrap css
 import "bootstrap/dist/css/bootstrap.css";
 
 const app = createApp(App);
+const pinia = createPinia();
 
 // middleware
 app.use(router);
+app.use(pinia);
 
 app.mount("#app");
 
