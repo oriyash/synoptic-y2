239ec3a56b8bdbb7d1e193e6d3e2715b10ec3cda
Yash Ori <yash.ori498@gmail.com>
2022-06-15 01:17:21 +0100

    added delete and border

--- client/src/components/Admin.vue
+++ client/src/components/Admin.vue
@@ -1,18 +1,26 @@
 <template>
     <div class="container mt-3 p-4">
-        <h1 class="text-warning">Here are all the currently reported fires:</h1>
+        <h1 class="text-danger mb-3">
+            Here are all the currently reported fires:
+        </h1>
         <div>
-            <div v-for="fire in fires" :key="fire" class="a-fire mb-2 p-3">
+            <div v-for="fire in fires" :key="fire" class="a-fire mb-3 p-3">
+                <span>{{ fire.desc }}</span> <br />
                 <span>Fire reported at {{ fire.createdAt }}, </span>
-                <span>Located at {{ fire.lat }}, {{ fire.lng }}</span>
+                <span>Located at {{ fire.lat }}, {{ fire.lng }}</span> <br />
                 <button class="btn btn-primary" @click="showImage(fire._id)">
                     Image
                 </button>
+                |
                 <button
                     class="btn btn-secondary"
                     @click="showLocation(fire.lat, fire.lng)"
                 >
                     Location
+                </button>
+                |
+                <button class="btn btn-danger" @click="onDelete(fire._id)">
+                    Delete
                 </button>
             </div>
         </div>
@@ -32,7 +40,7 @@ export default {
     async mounted() {
         try {
             const res = await axios.get("http://localhost:5000/report");
-            this.fires = res.data;
+            this.fires = res.data.reverse();
         } catch (error) {
             console.log(error);
         }
@@ -46,6 +54,19 @@ export default {
             window.open(
                 `https://www.google.com/maps/search/?api=1&query=${lat}%2C${lng}`
             );
+        },
+        async onDelete(fireId) {
+            try {
+                await axios.delete(`http://localhost:5000/report/${fireId}`);
+                try {
+                    const res = await axios.get("http://localhost:5000/report");
+                    this.fires = res.data.reverse();
+                } catch (error) {
+                    console.log(error);
+                }
+            } catch (error) {
+                console.log(error);
+            }
         },
     },
 };
@@ -62,5 +83,7 @@ export default {
 .a-fire {
     background-color: var(--bs-light);
     border: solid;
+    border-color: var(--bs-danger);
+    border-radius: 10px;
 }
 </style>
